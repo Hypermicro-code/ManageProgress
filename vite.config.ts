901bf092606d9f80c286646a92868aeff7fb33f0
Tony Mathisen <tony2mat@gmail.com>
2025-10-26 16:53:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // Minimal config for React + TS
 export default defineConfig({
   plugins: [react()],
+  // Using a relative base ensures the app works when served from GitHub Pages
+  // where it is hosted from a sub-path instead of the domain root.
+  base: './',
 });
